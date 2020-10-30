97df0130d467cee002553975087766b216fe92f0
Julian Gutierrez <julian_m_gtz@yahoo.com>
2020-10-29 20:13:49 -0500

    Added another program
    This is what I have been working on

---
+++ Hardware-Shop/Hardware-Shop/Hardware-Shop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hardware_Shop
+{
+    public partial class Hardware_Shop : Form
+    {
+        public Hardware_Shop()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCustomer_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            LoginPage lp = new LoginPage();
+            lp.Show();
+        }
+    }
+}
