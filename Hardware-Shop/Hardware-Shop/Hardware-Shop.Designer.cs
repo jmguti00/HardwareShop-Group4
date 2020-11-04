46398caa69c8787c25276a80f834e4e719957a30
Julian Gutierrez <julian_m_gtz@yahoo.com>
2020-11-04 00:20:20 -0600

    added the folder again with the changes
    added the folder again with the changes

---
+++ Hardware-Shop/Hardware-Shop/Hardware-Shop.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace Hardware_Shop
+{
+    partial class Hardware_Shop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSupervisor = new System.Windows.Forms.Button();
+            this.btnManager = new System.Windows.Forms.Button();
+            this.btnReports = new System.Windows.Forms.Button();
+            this.btnContactNumbers = new System.Windows.Forms.Button();
+            this.btnVisionStatement = new System.Windows.Forms.Button();
+            this.btnMissionStatement = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnCustomer = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.GhostWhite;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(376, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(417, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Andy\'s Hardware Store";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnSupervisor
+            // 
+            this.btnSupervisor.BackColor = System.Drawing.Color.GhostWhite;
+            this.btnSupervisor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSupervisor.Location = new System.Drawing.Point(168, 201);
+            this.btnSupervisor.Name = "btnSupervisor";
+            this.btnSupervisor.Size = new System.Drawing.Size(210, 37);
+            this.btnSupervisor.TabIndex = 2;
+            this.btnSupervisor.Text = "Supervisor";
+            this.btnSupervisor.UseVisualStyleBackColor = false;
+            // 
+            // btnManager
+            // 
+            this.btnManager.BackColor = System.Drawing.Color.GhostWhite;
+            this.btnManager.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnManager.Location = new System.Drawing.Point(168, 303);
+            this.btnManager.Name = "btnManager";
+            this.btnManager.Size = new System.Drawing.Size(210, 37);
+            this.btnManager.TabIndex = 3;
+            this.btnManager.Text = "Manager";
+            this.btnManager.UseVisualStyleBackColor = false;
+            this.btnManager.Click += new System.EventHandler(this.btnManager_Click);
+            // 
+            // btnReports
+            // 
+            this.btnReports.BackColor = System.Drawing.Color.GhostWhite;
+            this.btnReports.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReports.Location = new System.Drawing.Point(168, 397);
+            this.btnReports.Name = "btnReports";
+            this.btnReports.Size = new System.Drawing.Size(210, 37);
+            this.btnReports.TabIndex = 4;
+            this.btnReports.Text = "Reports";
+            this.btnReports.UseVisualStyleBackColor = false;
+            // 
+            // btnContactNumbers
+            // 
+            this.btnContactNumbers.BackColor = System.Drawing.Color.GhostWhite;
+            this.btnContactNumbers.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnContactNumbers.Location = new System.Drawing.Point(792, 397);
+            this.btnContactNumbers.Name = "btnContactNumbers";
+            this.btnContactNumbers.Size = new System.Drawing.Size(210, 37);
+            this.btnContactNumbers.TabIndex = 8;
+            this.btnContactNumbers.Text = "Contact Numbers";
+            this.btnContactNumbers.UseVisualStyleBackColor = false;
+            // 
+            // btnVisionStatement
+            // 
+            this.btnVisionStatement.BackColor = System.Drawing.Color.GhostWhite;
+            this.btnVisionStatement.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVisionStatement.Location = new System.Drawing.Point(792, 303);
+            this.btnVisionStatement.Name = "btnVisionStatement";
+            this.btnVisionStatement.Size = new System.Drawing.Size(210, 37);
+            this.btnVisionStatement.TabIndex = 7;
+            this.btnVisionStatement.Text = "Vision Statement";
+            this.btnVisionStatement.UseVisualStyleBackColor = false;
+            // 
+            // btnMissionStatement
+            // 
+            this.btnMissionStatement.BackColor = System.Drawing.Color.GhostWhite;
+            this.btnMissionStatement.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMissionStatement.Location = new System.Drawing.Point(792, 201);
+            this.btnMissionStatement.Name = "btnMissionStatement";
+            this.btnMissionStatement.Size = new System.Drawing.Size(210, 37);
+            this.btnMissionStatement.TabIndex = 6;
+            this.btnMissionStatement.Text = "Mission Statement";
+            this.btnMissionStatement.UseVisualStyleBackColor = false;
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.BackColor = System.Drawing.Color.GhostWhite;
+            this.btnHelp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHelp.Location = new System.Drawing.Point(792, 101);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(210, 37);
+            this.btnHelp.TabIndex = 5;
+            this.btnHelp.Text = "Helpful Info";
+            this.btnHelp.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightYellow;
+            this.panel1.Controls.Add(this.lblInfo);
+            this.panel1.Location = new System.Drawing.Point(168, 462);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(834, 233);
+            this.panel1.TabIndex = 10;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.BackColor = System.Drawing.Color.GhostWhite;
+            this.lblInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblInfo.Location = new System.Drawing.Point(14, 15);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(801, 197);
+            this.lblInfo.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Black;
+            this.panel4.Location = new System.Drawing.Point(143, -1);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(895, 16);
+            this.panel4.TabIndex = 13;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Black;
+            this.panel5.Location = new System.Drawing.Point(143, 717);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(895, 22);
+            this.panel5.TabIndex = 12;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Location = new System.Drawing.Point(0, -1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(146, 740);
+            this.panel2.TabIndex = 11;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Black;
+            this.panel3.Location = new System.Drawing.Point(1034, -1);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(146, 740);
+            this.panel3.TabIndex = 12;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Hardware_Shop.Properties.Resources.HandyAndy;
+            this.pictureBox1.Location = new System.Drawing.Point(398, 101);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(376, 333);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnCustomer
+            // 
+            this.btnCustomer.BackColor = System.Drawing.Color.GhostWhite;
+            this.btnCustomer.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnCustomer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCustomer.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnCustomer.Image = global::Hardware_Shop.Properties.Resources.Home_Test2;
+            this.btnCustomer.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCustomer.Location = new System.Drawing.Point(168, 87);
+            this.btnCustomer.Name = "btnCustomer";
+            this.btnCustomer.Size = new System.Drawing.Size(181, 51);
+            this.btnCustomer.TabIndex = 1;
+            this.btnCustomer.Text = "Customer";
+            this.btnCustomer.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCustomer.UseVisualStyleBackColor = false;
+            this.btnCustomer.Click += new System.EventHandler(this.btnCustomer_Click);
+            // 
+            // Hardware_Shop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.ClientSize = new System.Drawing.Size(1182, 738);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnContactNumbers);
+            this.Controls.Add(this.btnVisionStatement);
+            this.Controls.Add(this.btnMissionStatement);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.btnReports);
+            this.Controls.Add(this.btnManager);
+            this.Controls.Add(this.btnSupervisor);
+            this.Controls.Add(this.btnCustomer);
+            this.Controls.Add(this.label1);
+            this.Name = "Hardware_Shop";
+            this.Text = "Andy\'s Hardware Store";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCustomer;
+        private System.Windows.Forms.Button btnSupervisor;
+        private System.Windows.Forms.Button btnManager;
+        private System.Windows.Forms.Button btnReports;
+        private System.Windows.Forms.Button btnContactNumbers;
+        private System.Windows.Forms.Button btnVisionStatement;
+        private System.Windows.Forms.Button btnMissionStatement;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ Hardware-Shop/Hardware-Shop/LoginPage.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Hardware_Shop
+{
+    public partial class LoginPage : Form
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void lblForgotPassword_Click(object sender, EventArgs e)
+        {
+            SendCode sc = new SendCode();
+            this.Hide();
+            sc.Show();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection("Data Source=DESKTOP-BNB45KS\\MSSQLSERVER1;Initial Catalog=HandyHardwareShop;Integrated Security=True");
+                SqlDataAdapter sda = new SqlDataAdapter("SELECT * FROM group4.LoginInfo WHERE User_Name = '" + txtUserName.Text + "' and User_Password = '" + txtPassword.Text + "' ", con);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+
+                if (dt.Rows.Count > 0)
+                {
+                    MessageBox.Show("Login successfully");
+
+                    if (Hardware_Shop.User.userName == "Customer")
+                    {
+                        this.Hide();
+
+                        CustomerPage cp = new CustomerPage();
+                        cp.Show();
+                    }
+                    else if (Hardware_Shop.User.userName == "Manager")
+                    {
+                        this.Hide();
+
+                        Add_Product_And_Picture addProductPicture = new Add_Product_And_Picture();
+                        addProductPicture.Show();
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Wrong password or user ID");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            
+        }
+    }
+}
