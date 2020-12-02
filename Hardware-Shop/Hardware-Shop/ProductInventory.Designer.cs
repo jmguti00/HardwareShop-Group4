e123de2952be2eb3176c13fa4813c721c1654873
Julian Gutierrez <julian_m_gtz@yahoo.com>
2020-12-02 00:05:58 -0600

    Made changes to code of ProductInvenotry form and Crystal report

--- Hardware-Shop/Hardware-Shop/ProductInventory.Designer.cs
+++ Hardware-Shop/Hardware-Shop/ProductInventory.Designer.cs
@@ -35,12 +35,13 @@
             // btnMain
             // 
             this.btnMain.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMain.Location = new System.Drawing.Point(291, 23);
+            this.btnMain.Location = new System.Drawing.Point(463, 12);
             this.btnMain.Name = "btnMain";
             this.btnMain.Size = new System.Drawing.Size(157, 33);
             this.btnMain.TabIndex = 0;
             this.btnMain.Text = "Main Menu";
             this.btnMain.UseVisualStyleBackColor = true;
+            this.btnMain.Click += new System.EventHandler(this.btnMain_Click);
             // 
             // crystalReportViewer1
             // 
@@ -49,7 +50,7 @@
             this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
             this.crystalReportViewer1.Location = new System.Drawing.Point(12, 62);
             this.crystalReportViewer1.Name = "crystalReportViewer1";
-            this.crystalReportViewer1.Size = new System.Drawing.Size(776, 376);
+            this.crystalReportViewer1.Size = new System.Drawing.Size(1154, 642);
             this.crystalReportViewer1.TabIndex = 1;
             this.crystalReportViewer1.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None;
             // 
@@ -57,7 +58,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1197, 716);
             this.Controls.Add(this.crystalReportViewer1);
             this.Controls.Add(this.btnMain);
             this.Name = "ProductInventory";
