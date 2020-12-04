e61a9619c42e78f9e78278e199470320f8996781
kcfurlow <71146313+kcfurlow@users.noreply.github.com>
2020-12-04 15:40:44 -0600

    Add files via upload
    Adding Place Order and all associated forms.

---
+++ PlaceOrder.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hardware_Shop
+{
+    public partial class PlaceOrder : Form
+    {
+        public PlaceOrder()
+        {
+            InitializeComponent();
+        }
+
+        private void PlaceOrder_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnToolsAccessories_Click(object sender, EventArgs e)
+        {
+            ToolsAndAccessories TaA = new ToolsAndAccessories();
+            TaA.Show();
+            this.Hide();
+        }
+
+        private void btnHomeReno_Click(object sender, EventArgs e)
+        {
+            HomeRenovations homeRenovations = new HomeRenovations();
+            homeRenovations.Show();
+            this.Hide();
+        }
+
+        private void btnLawnGarden_Click(object sender, EventArgs e)
+        {
+            LawnAndGarden lawnAndGarden = new LawnAndGarden();
+            lawnAndGarden.Show();
+            this.Hide();
+        }
+
+        private void btnBuildingConstruction_Click(object sender, EventArgs e)
+        {
+            BuildingAndConstuction BaC = new BuildingAndConstuction();
+            BaC.Show();
+            this.Hide();
+        }
+
+        private void btnReturnToCust_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            CustomerPage cp = new CustomerPage();
+            cp.Show();
+        }
+    }
+}
---
+++ ViewCart.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hardware_Shop
+{
+    public partial class ViewCart : Form
+    {
+        CheckOutForm cof = new CheckOutForm();
+        
+        public class item
+        {
+            float total = 0;
+            public string itemDescription { get; set; }
+            public string itemName { get; set; }
+            public float itemCost { get; set; }
+            public int itemStock { get; set; }
+
+            //remove from lstBox
+            public void removeItem()
+            {
+                total = total - itemCost;
+            }
+
+            public override string ToString()
+            {
+                return itemName + " " + itemCost;
+            }
+        }
+
+        public ViewCart()
+        {
+            InitializeComponent();
+        }
+
+        private void ViewCart_Load(object sender, EventArgs e)
+        {
+            //string item = string.Join(" ", ToolsAndAccessories.cart);
+            //int x = 0;
+
+            ////loads each item from cart into lstOrderSummary
+            //foreach(ToolsAndAccessories.item i in ToolsAndAccessories.cart)
+            //{
+            //    x++;
+            //    lstOrderSummary.Items.Add(item + " " + x);
+            //}
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            
+            ToolsAndAccessories.item i = new ToolsAndAccessories.item();
+            i = (ToolsAndAccessories.item)lstOrderSummary.SelectedItem;
+            int index = lstOrderSummary.SelectedIndex;
+
+            if (index >= 0)
+            {
+                lstOrderSummary.Items.RemoveAt(index);
+                ToolsAndAccessories.total -= i.itemCost;
+                lblTotal.Text = ToolsAndAccessories.total.ToString("C2");
+                
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lstOrderSummary.Items.Clear();
+            lblTotal.Text = "";
+            ToolsAndAccessories.total = 0;
+            ToolsAndAccessories.cart.Clear();
+            cof.lstOrderSummary.Items.Clear();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void btnMainMenu_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            ToolsAndAccessories t = new ToolsAndAccessories();
+            t.Show();
+        }
+
+    }
+}
